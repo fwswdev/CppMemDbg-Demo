695d8739bc8130a9fd01255b65716660a23442e9
killer@killer.com <killer@killer.com>
2015-10-11 12:12:47 +0800

    added boost smart pointers. in progress

--- memleakdetect/memleakdetect.cpp
+++ memleakdetect/memleakdetect.cpp
@@ -5,8 +5,13 @@
 #include <Windows.h>
 #include <iostream>
 #include <string>
+#include <boost/shared_ptr.hpp>
+#include <boost/weak_ptr.hpp>
 using namespace std;
 #include "cppMemDbg.h"
+
+
+#define SHARED_PTR  boost::shared_ptr // TODO: use typedef on this one
 
 class TestClass
 {
@@ -28,6 +33,45 @@ public:
 
 private:
 	int *x;
+};
+
+
+class Child; // forward reference
+
+class Parent
+{
+public:
+	Parent(string name, SHARED_PTR<Child> child)
+	{
+		this->child = child;
+		this->name = name;
+	}
+
+private:
+	SHARED_PTR<Child> child;
+	string name;
+};
+
+class Child
+{
+public:
+	Child(string name,SHARED_PTR<Parent> mom, SHARED_PTR<Parent> dad)
+	{
+		this->mom = mom;
+		this->dad = dad;
+		this->name = name;
+		cout << "Child " << name << " constructor";
+	}
+
+	~Child()
+	{
+		cout << "Child " << name << " destructor";
+	}
+
+private:
+	SHARED_PTR<Parent> mom;
+	SHARED_PTR<Parent> dad;
+	string name;
 };
 
 
